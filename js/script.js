3785506506a34ce7c26aab364b246982894cf7af
yared-abera <yaredoabera7@gmail.com>
2024-06-01 19:33:38 +0300

    this is part 1 commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
