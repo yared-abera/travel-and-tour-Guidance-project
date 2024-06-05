1be6012faf1b82162f75f907805aaebef05d530a
yared-abera <yaredoabera7@gmail.com>
2024-06-05 20:47:42 +0300

    package part

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Travel Agency :: Best Agency</title>
+
+   <!-- swiper css link 
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" /> -->
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css\package.css">
+
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo"><img src="images/logo.jpg"></a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php" class="active">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/package-bg.jpg) no-repeat">
+   
+   <h1>packages</h1>
+ 
+ </div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/lalibela.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Lalibela Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Lalibela top selling packages!</p>
+            <h2>$50</h2> 
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/Debere-Libanos.jpg" alt="Debere-Libanos">
+         </div>
+         <div class="content">
+            <h3>Debre Libanos Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Debre Libanos top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/souf-omer.jpg" alt="souf-omer">
+         </div>
+         <div class="content">
+            <h3>sof omer Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our souf omer top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/walia.jpg" alt="Walia">
+         </div>
+         <div class="content">
+            <h3> Walia Ibex Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Park top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/aksum.jpg" alt="Aksum">
+         </div>
+         <div class="content">
+            <h3> Aksum Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Aksum top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/fasil.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Fasil Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Fasiledes top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/jagol.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Jagol Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Jagol wall Harar top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/dalol.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Dalol Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Dallol top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/Aba jifar palace.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Aba jifar palace Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Aba jifar palace top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/omo valley.webp" alt="">
+         </div>
+         <div class="content">
+            <h3>  omo valley Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our  omo valley top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/tiya.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Tiya and Melka Kuntre Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Tiya and Melka Kuntre top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/Nechisar park.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Nechisar park Tour Packages</h3>
+            <p>Enjoy the Ethiopia with unforgettable fun with our Nechisar park top selling packages!</p>
+            <h2>$50</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">see more</span></div>
+<button type="button" class="scroll-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+</body>
+</html>
