cc1fafba68557814e175c43a9b0c9ea22cf8ed9b
yared-abera <yaredoabera7@gmail.com>
2024-06-05 14:06:01 +0300

    first commit

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
