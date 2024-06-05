34bde7db8024c0167e04bb187c82c9baedc0b401
Andebet3210 <andebetmolla1900@gmail.com>
2024-06-05 23:27:46 +0300

    this is the commit of updated css

--- book.php
+++ book.php
@@ -48,35 +48,35 @@
             <div class="flex">
                 <div class="inputBox">
                     <span>name :</span>
-                    <input type="text" placeholder="enter your name" name="name">
+                    <input type="text" placeholder="enter your name" name="name" required="required">
                 </div>
                 <div class="inputBox">
                     <span>email :</span>
-                    <input type="email" placeholder="enter your email" name="email">
+                    <input type="email" placeholder="enter your email" name="email" required="required">
                 </div>
                 <div class="inputBox">
                     <span>phone :</span>
-                    <input type="number" placeholder="enter your number" name="phone">
+                    <input type="number" placeholder="enter your number" name="phone" required="required">
                 </div>
                 <div class="inputBox">
                     <span>address :</span>
-                    <input type="text" placeholder="enter your address" name="address">
+                    <input type="text" placeholder="enter your address" name="address" required="required">
                 </div>
                 <div class="inputBox">
                     <span>where to :</span>
-                    <input type="text" placeholder="place you want to visit" name="location">
+                    <input type="text" placeholder="place you want to visit" name="location" required="required">
                 </div>
                 <div class="inputBox">
                     <span>how many :</span>
-                    <input type="number" placeholder="number of guests" name="guests">
+                    <input type="number" placeholder="number of guests" name="guests" required="required">
                 </div>
                 <div class="inputBox">
                     <span>arrivals :</span>
-                    <input type="date" name="arrivals">
+                    <input type="date" name="arrivals" required="required">
                 </div>
                 <div class="inputBox">
                     <span>leaving :</span>
-                    <input type="date" name="leaving">
+                    <input type="date" name="leaving" required="required">
                 </div>
             </div>
 
