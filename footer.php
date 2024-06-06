cbdfda37dbe0d17daf4e4b782ea503c8713d4071
abiyandargie <andargieabiy85@gmail.com>
2024-06-05 17:23:36 -0700

    commiting footer php

---
+++ footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+<section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+            </div>
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            </div>
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +251911000001 </a>
+                <a href="#"> <i class="fas fa-phone"></i> +251707070707 </a>
+                <a href="#"> <i class="fas fa-envelope"></i> travelethio@gmail.com </a>
+                <a href="#"> <i class="fas fa-map"></i> Addiss Ababa,Ethiopia </a>
+            </div>
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            </div>
+        </div>
+        <div class="credit"> Developed by Travel Group</div>
+    </section>
+    </body>
+</html>
