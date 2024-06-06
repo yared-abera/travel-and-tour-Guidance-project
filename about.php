ae8b447dad06ae7722ab7ac44e1def76ee7d62eb
Meskerem3 <eyerusalemdegu@gmail.com>
2024-06-06 14:07:13 +0300

    about section

---
+++ about.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Travel Agency :: Best Agency</title>
+   
+
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link rel="stylesheet" href="css/about.css">
+</head>
+<body>
+   <div class="heading" style="background:url(images/blo.png)  no-repeat">
+   
+</div>
+   <section class="about">
+
+   <div class="image">
+      <img src="images/simen.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>Amazing Tours a Best tour Operator and Travel agent in Ethiopia. We are Tour Package, Hotel booking. Ethiopia Tours & Travel agent. Find international and domestic tour packages from bangladesh at low prices including best ... Travel Package | Honeymoon Package | Tour Package
+      Fantastic getaway. Very good communication. Friendly and Easy to deal with.</p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>reasonable price</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+<section class="reviews">
+
+   <h1 class="heading-title"> OUR TOUR GUIDERS </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>The best well known tour guider in ethiopia best known in ethiopian wild life.</p>
+            <h3>AZIZ AHMED</h3>
+            <span>Tour Guider</span>
+            <img src="images/aziz ahmed.jfif" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Most experienced person in this company having a great knowledge in historical part of ethiopia</p>
+            <h3>KEBROM SEIFU</h3>
+            <span>Tour Guider</span>
+            <img src="images/kebrom.jfif" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>most experienced in northern part of ethiopia</p>
+            <h3>ABEBE TEKA</h3>
+            <span>Tour Guider</span>
+            <img src="images/abebe.jfif" alt="">
+         </div>
+
+         
+
+      </div>
+
+   </div>
+</body>
+</html>
