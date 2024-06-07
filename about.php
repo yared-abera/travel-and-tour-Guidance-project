fb878c7a1141269334a55c5453443f4cf8b98871
Meskerem3 <eyerusalemdegu@gmail.com>
2024-06-07 14:48:12 +0300

    about section footer added

--- about.php
+++ about.php
@@ -10,9 +10,21 @@
    
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
    <link rel="stylesheet" href="css/about.css">
+    <script>
+       $(document).ready(function(){
+           $(".scroll-top").click(function() {
+               $("html, body").animate({ 
+                  scrollTop: 0 
+             }, "slow");
+               return false;
+        });
+       });
+   </script>
 </head>
 <body>
+   
    <div class="heading" style="background:url(images/blo.png)  no-repeat">
+      <h1>ABOUT US</h1>
   
    
 </div>
@@ -43,58 +55,12 @@
    </div>
 
 </section>
-<section class="reviews">
 
-   <h1 class="heading-title"> OUR TOUR GUIDERS </h1>
 
-   <div class="swiper reviews-slider">
 
-      <div class="swiper-wrapper">
-
-         <div class="swiper-slide">
-            <div class="stars">
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-            </div>
-            <p>The best well known tour guider in ethiopia best known in ethiopian wild life.</p>
-            <h3>AZIZ AHMED</h3>
-            <span>Tour Guider</span>
-            <img src="images/aziz ahmed.jfif" alt="">
-         </div>
-
-         <div class="swiper-slide">
-            <div class="stars">
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-            </div>
-            <p>Most experienced person in this company having a great knowledge in historical part of ethiopia</p>
-            <h3>KEBROM SEIFU</h3>
-            <span>Tour Guider</span>
-            <img src="images/kebrom.jfif" alt="">
-         </div>
-
-         <div class="swiper-slide">
-            <div class="stars">
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-            </div>
-            <p>most experienced in northern part of ethiopia</p>
-            <h3>ABEBE TEKA</h3>
-            <span>Tour Guider</span>
-            <img src="images/abebe.jfif" alt="">
-         </div>
-
-         
-
-      </div>
-
-   </div>
-</section>
+<script src="js/script.js"></script>
+<?php
+include "footer.php";
+?>
 </body>
 </html>
