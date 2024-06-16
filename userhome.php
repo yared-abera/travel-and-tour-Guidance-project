3ea26dc0831bcdb02de5fef5bdd932f5aacb6916
Andebet3210 <andebetmolla1900@gmail.com>
2024-06-17 00:42:06 +0300

    this is the commit of modified css and admin home

--- userhome.php
+++ userhome.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>user home</title>
+    <link rel="stylesheet" href="css/userhome.css">
 </head>
+
 <body>
-    <?php
-    include("header.php");
-    ?>
+    <section class="header">
+        <div class="home">
+            <div class="div">
+                <a href="">Profile</a>
+                <a href="">Change Password</a>
+                <a href="">History</a>
+            </div>
+            <a href="">Logout</a>
+
+        </div>
+    </section>
+
 </body>
+
 </html>
