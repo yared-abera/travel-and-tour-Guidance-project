b4e14023caf5a56f32b82031a85c7c91b2aae001
abiyandargie <andargieabiy85@gmail.com>
2024-06-16 18:35:28 -0700

    some updation on

--- delete_package.php
+++ delete_package.php
@@ -1,12 +1,17 @@
 <?php
-require"dbconnect.php";
+require "dbconnect.php";
 $id = $_POST['id'];
 
 $sql = "DELETE FROM packages WHERE package_id='$id'";
 if ($conn->query($sql) === TRUE) {
     echo "Record deleted successfully";
 } else {
-    echo "Error deleting record: " . $conn->error;
+    // Check if the error is due to a foreign key constraint
+    if (strpos($conn->error, 'foreign key constraint') !== false) {
+        echo "Error deleting record: The package is referenced by one or more bookings and cannot be deleted.";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
 }
 
 $conn->close();
