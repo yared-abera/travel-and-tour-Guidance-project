99602868911e5b9747e792cfcc8071b91a52f192
abiyandargie <andargieabiy85@gmail.com>
2024-06-16 17:22:49 -0700

    updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+      node: true,
+      browser: true,
+      es2021: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
     },
-    "rules": {
-    }
-};
+    rules: {},
+  };
