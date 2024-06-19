478e62c5e655486d0e9c5f68002a75707d9b2d56
yared-abera <yaredoabera7@gmail.com>
2024-06-19 11:04:53 +0300

    modified about

--- about.php
+++ about.php
@@ -1,3 +1,18 @@
+<?php 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+ 
+// Check if the user is logged in
+if (isset($_SESSION['username'])) { 
+    header("Location: userhome.php");
+}
+  
+   include("above_header.php"); 
+   include("header.php");
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
