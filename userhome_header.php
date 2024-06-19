aa1d669463d815d8a7d9cc055e9defe6350a1d46
yared-abera <yaredoabera7@gmail.com>
2024-06-19 08:40:25 +0300

    added  userhome_header.php

---
+++ userhome_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user home</title>
+    <link rel="stylesheet" href="css/userhome.css">
+</head>
+<body>
+    <section class="header">
+        <div class="home">
+            <div class="div">
+                <a href="userprofile.php">Profile</a>
+                <a href="">Change Password</a>
+                <a href="user-booking-his.php">History</a>
+            </div>
+            <a href="logout.php">Logout</a>
+
+        </div>
+    </section>
+    
+</body>
+
+</html>
