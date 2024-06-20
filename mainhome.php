8280930d1e3a13143ea512d77c68278310d26c3f
yared-abera <yaredoabera7@gmail.com>
2024-06-20 10:06:10 +0300

    modified version

--- mainhome.php
+++ mainhome.php
@@ -15,7 +15,8 @@
    <link rel="stylesheet" href="css/package.css">
    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
      <link rel="stylesheet" href="css/style.css">
-    <script>
+   
+   <script>
        $(document).ready(function(){
            $(".scroll-top").click(function() {
                $("html, body").animate({ 
